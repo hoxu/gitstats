385019750a703b98553eb1d8a2f42e6c6e6b7bb7
Schultz <GSchultz@zebra.com>
2017-07-06 17:29:10 -0400

    execution py for easy build

---
+++ execution.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+# python execution.py <source folder> <output folder>
+def generateGitstatsOnFolders (): 
+	# print (sys.argv)
+
+	#  root location
+	source_folder = sys.argv[1]
+	# final destination
+	output_folder = sys.argv[2]
+	print (source_folder)
+
+	dirs_name=[]
+	subdir_name=''
+	
+	for subdir, dirs, files in os.walk(source_folder):
+		dirs_name=dirs
+		subdir_name=subdir
+		# weirdly keeps looping...
+		break;
+	# print (dirs_name)
+	for direct in dirs_name:
+		fullPath=(os.path.join(subdir_name, direct))
+		os.system("./gitstats "+fullPath+" "+output_folder)
+
+
+if __name__ == "__main__":
+	generateGitstatsOnFolders()
